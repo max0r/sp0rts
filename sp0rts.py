4ba844da22fbfe127af92591578cc8f675bf5578
Max Leonard <max@tripfire.org>
2023-05-11 20:44:48 -0500

    work in progress

--- sp0rts.py
+++ sp0rts.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/opt/homebrew/bin/python3
 
 import urllib.request
 from bs4 import BeautifulSoup
@@ -12,6 +12,11 @@ fp.close()
 soup = BeautifulSoup(content, 'html.parser')
 sp0rts = []
 for a in soup.find_all('table'):
-    print('found table:')
     for x in a.find('th', 'wtwmatchuptitle'):
         print('CATEGORY: ' + x)
+    for y in a.find('td', 'wtwmatchup'):
+        print('EVENT: ' + y)
+    for z in a.find('td', 'wtwtime'):
+        print('TIME: ' + z)
+    for tv in a.find('td', 'wtwtvchannel'):
+        print('TV: ' + tv)
