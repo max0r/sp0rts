4d5c7ebb4c6c79778bb0e2802966fec88241c32b
max0r <max@tripfire.org>
2023-05-14 21:20:22 -0500

    Update sp0rts.py
    icons and tabulation!

--- sp0rts.py
+++ sp0rts.py
@@ -1,7 +1,14 @@
 #!/opt/homebrew/bin/python3
 
 import urllib.request
+from collections import OrderedDict
 from bs4 import BeautifulSoup
+from tabulate import tabulate
+
+emojis = {
+    'MLB' : '⚾️',
+    'NBA' : '🏀'
+        }
 
 fp = urllib.request.urlopen("https://sportsgamestoday.com/tv/what-to-watch.php")
 mybytes = fp.read()
@@ -11,12 +18,20 @@ fp.close()
 
 soup = BeautifulSoup(content, 'html.parser')
 sp0rts = []
+print('sp0rts:')
 for a in soup.find_all('table'):
+    event = OrderedDict()
     for x in a.find('th', 'wtwmatchuptitle'):
-        print('CATEGORY: ' + x)
+        event['category'] = x
+        event['icon'] = emojis.get(x)
     for y in a.find('td', 'wtwmatchup'):
-        print('EVENT: ' + y)
+        event['title'] = y.strip()
     for z in a.find('td', 'wtwtime'):
-        print('TIME: ' + z)
+        event['time'] = z
+    event['chans'] = []
     for tv in a.find('td', 'wtwtvchannel'):
-        print('TV: ' + tv.text)
+        if tv.find('br'):
+            event['chans'].append(tv)
+    sp0rts.append(event)
+print(tabulate(sp0rts, tablefmt='github'))
+print(sp0rts)
